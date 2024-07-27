1eeed5da5a2d8c651d93a3461b550924e39002ce
Venzelland <101016773+Venzelland@users.noreply.github.com>
2024-07-27 22:54:13 +0300

    add routers

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
 import IncomeForm from './components/IncomeForm';
 import IncomeList from './components/IncomeList';
 import ExpenseForm from './components/ExpenseForm';
@@ -8,12 +9,35 @@ const App = () => {
     return (
         <div>
             <h1>Семейный бюджет</h1>
-            <IncomeList />
-            <IncomeForm />
-            <ExpenseList />
-            <ExpenseForm />
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/income-list">Income List</Link>
+                    </li>
+                    <li>
+                        <Link to="/income-form">Income Form</Link>
+                    </li>
+                    <li>
+                        <Link to="/expense-list">Expense List</Link>
+                    </li>
+                    <li>
+                        <Link to="/expense-form">Expense Form</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/income-list" element={<IncomeList />} />
+                <Route path="/income-form" element={<IncomeForm />} />
+                <Route path="/expense-list" element={<ExpenseList />} />
+                <Route path="/expense-form" element={<ExpenseForm />} />
+                <Route path="/" element={<Home />} />
+            </Routes>
         </div>
     );
+};
+
+const Home = () => {
+    return <h2>Welcome to Family Budget App</h2>;
 };
 
 export default App;
